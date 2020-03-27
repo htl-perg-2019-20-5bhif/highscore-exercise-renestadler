673277ab02446b3219175439867702f47fabc4ec
Michael Hitzker <hitzker.michael@gmail.com>
2020-03-27 13:50:57 +0100

    Added frontend stuff

--- ClientSide/webpack.common.js
+++ ClientSide/webpack.common.js
@@ -1,39 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
   context: path.join(__dirname, "src"),
-  entry: './index.ts',
+  entry: "./index.ts",
   performance: { hints: false },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: [".tsx", ".ts", ".js"]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: 'index.html'
+      template: "index.html"
     }),
-    new CopyPlugin([
-      { from: 'images', to: 'images' },
-    ]),
+    new CopyPlugin([{ from: "images", to: "images" }]),
+    new CopyPlugin([{ from: "fonts", to: "fonts" }]),
     new ESLintPlugin({
-      files: '*.ts'
-    }),
+      files: "*.ts"
+    })
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
 };
